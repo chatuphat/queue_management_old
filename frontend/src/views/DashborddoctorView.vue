4b6d46e94716ff4bb81a81f1b59e749fddf5b86d
chatuphat laosomboon <63543816+chatuphat@users.noreply.github.com>
2023-03-30 13:13:42 +0700

    edit logo title web

--- frontend/src/views/DashborddoctorView.vue
+++ frontend/src/views/DashborddoctorView.vue
@@ -183,7 +183,7 @@
             </div>
 
             <div class="mx-5">
-              <h4 class="text-2xl font-semibold text-gray-700">50</h4>
+              <h4 class="text-2xl font-semibold text-gray-700">25</h4>
               <div class="text-gray-500">เร่งด่วน</div>
             </div>
           </div>

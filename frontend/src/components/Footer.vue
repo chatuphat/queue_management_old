c19a4ac02c6a334a63843c6ac43171c2d1e6fa3c
chatuphat laosomboon <63543816+chatuphat@users.noreply.github.com>
2023-03-31 16:17:52 +0700

    edit bug footer.vue

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="w-full border-t-2 px-8 py-3 lg:flex justify-between items-center">
-    <!-- <p class="mb-2 lg:mb-0">© Copyright {{ currentYear }}</p> -->
-
-    <!-- <div class="flex">
-      <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
-      <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a>
-      <a href="#" class="hover:text-gray-900">About Us</a>
-    </div> -->
-  </div>
+  <div
+    class="w-full border-t-2 px-8 py-3 lg:flex justify-between items-center"
+  ></div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from "vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 
-const currentYear = ref<number>(new Date().getFullYear());
+export default defineComponent({
+  name: "PageFooter", // Renamed component to multi-word name
+  setup() {
+    return {};
+  },
+});
 </script>
